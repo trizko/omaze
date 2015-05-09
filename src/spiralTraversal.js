689bee5320df0d3b870c566cc68e14cc09396782
Tony Rizko <tony@rizkocircle.com>
2015-05-09 14:38:59 -0700

    (pass)removeTopRow tests

--- src/spiralTraversal.js
+++ src/spiralTraversal.js
@@ -4,4 +4,13 @@ spiral.takeTopRow = function(matrix){
   return matrix[0];
 };
 
+spiral.removeTopRow = function(matrix){
+  var resultMatrix = [];
+  for(var i = 1; i < matrix.length; i++){
+    resultMatrix.push(matrix[i]);
+  }
+
+  return resultMatrix;
+};
+
 module.exports = spiral;
