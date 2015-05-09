f1c09b58c9e5084063be5554281a56ecd677a528
Tony Rizko <tony@rizkocircle.com>
2015-05-09 14:06:03 -0700

    (refactor)spiralTraversal tests

--- test/spiralTraversal_test.js
+++ test/spiralTraversal_test.js
@@ -2,7 +2,9 @@ var spiral = require('../src/spiralTraversal');
 var assert = require('assert');
 
 describe('Spiral Traversal', function(){
-  it('should have a method for removing the top row of a matrix', function(){
-    assert.doesNotThrow(spiral.takeTopRow);
+  describe('takeTopRow()', function(){
+    it('should exist', function(){
+      assert.doesNotThrow(spiral.takeTopRow);
+    });
   });
 });
