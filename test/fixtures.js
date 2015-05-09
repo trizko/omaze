7a743846a3341790797865ddf1f1a293a4e12038
Tony Rizko <tony@rizkocircle.com>
2015-05-09 14:16:30 -0700

    (test)for takeTopRow method

---
+++ test/fixtures.js
@@ -0,0 +1,10 @@
+var fixtures = {};
+
+//fixtures for takeTopRow tests
+fixtures.takeTopRow = {};
+fixtures.takeTopRow.emptyMatrix = {
+  inputValue: [[]],
+  outputExpected: []
+}
+
+module.exports = fixtures;
--- test/spiralTraversal_test.js
+++ test/spiralTraversal_test.js
@@ -1,10 +1,16 @@
 var spiral = require('../src/spiralTraversal');
 var assert = require('assert');
+var fixtures = require('./fixtures.js')
 
 describe('Spiral Traversal', function(){
   describe('takeTopRow()', function(){
     it('should exist', function(){
       assert.doesNotThrow(spiral.takeTopRow);
+    });
+    it('should return an empty array if given an empty matrix', function(){
+      var result = spiral.takeTopRow(fixtures.takeTopRow.emptyMatrix.inputValue);
+      var expected = fixtures.takeTopRow.emptyMatrix.outputExpected;
+      assert.equal(result, expected);
     });
   });
 });
