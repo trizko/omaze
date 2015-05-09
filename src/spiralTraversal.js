6517f7ade0fd16befea267b08c858554b296204f
Tony Rizko <tony@rizkocircle.com>
2015-05-09 14:49:50 -0700

    (pass)takeRightColumn tests

--- src/spiralTraversal.js
+++ src/spiralTraversal.js
@@ -13,4 +13,17 @@ spiral.removeTopRow = function(matrix){
   return resultMatrix;
 };
 
+spiral.takeRightColumn = function(matrix){
+  var resultMatrix = [];
+
+  for(var i = 0; i < matrix.length; i++){
+    var lastElementIndex = matrix[i].length - 1;
+    if(lastElementIndex !== -1){
+      resultMatrix.push(matrix[i][lastElementIndex])
+    }
+  }
+
+  return resultMatrix;
+}
+
 module.exports = spiral;
