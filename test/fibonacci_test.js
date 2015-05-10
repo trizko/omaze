dce74c5c56f77e8d9444eb257c9bc1f1347c7977
Tony Rizko <tony@rizkocircle.com>
2015-05-09 19:07:20 -0700

    (test)for fib iterative method

--- test/fibonacci_test.js
+++ test/fibonacci_test.js
@@ -24,4 +24,27 @@ describe('Fibonacci', function(){
       assert.equal(result, expected);
     });
   });
+
+  describe('iterative', function(){
+    it('should exist', function(){
+      assert.doesNotThrow(function(){
+        fibonacci.iterative(0);
+      });
+    });
+    it('should return 5 if given 5 as nth input', function(){
+      var result = fibonacci.iterative(5);
+      var expected = 5;
+      assert.equal(result, expected);
+    });
+    it('should return 21 if given 8 as nth input', function(){
+      var result = fibonacci.iterative(8);
+      var expected = 21;
+      assert.equal(result, expected);
+    });
+    it('should return 144 if given 12 as nth input', function(){
+      var result = fibonacci.iterative(12);
+      var expected = 144;
+      assert.equal(result, expected);
+    });
+  });
 });
