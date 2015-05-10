80fa1864efb8d905c9123cde3595f7a316ca71c6
Tony Rizko <tony@rizkocircle.com>
2015-05-09 19:02:07 -0700

    (test)for fib recursion method

--- test/fibonacci_test.js
+++ test/fibonacci_test.js
@@ -1,12 +1,17 @@
-var spiral = require('../src/fibonacci.js');
+var fibonacci = require('../src/fibonacci.js');
 var assert = require('assert');
 
 describe('Fibonacci', function(){
-  describe('fibRecursion', function(){
+  describe('recursion', function(){
     it('should exist', function(){
       assert.doesNotThrow(function(){
         fibonacci.recursion(0);
       });
+    });
+    it('should return 5 if given 5 as nth input', function(){
+      var result = fibonacci.recursion(5);
+      var expected = 5;
+      assert.equal(result, expected);
     });
   });
 });
