69801a965f9e30e844dc8d027ad0ebde7161da5e
Tony Rizko <tony@rizkocircle.com>
2015-05-09 18:33:13 -0700

    (test)for traverseCounterClockwise method

--- test/fixtures.js
+++ test/fixtures.js
@@ -290,6 +290,65 @@ fixtures.traverseClockwise.fourByFive = {
   outputExpected: [1,2,3,4,5,10,15,20,19,18,17,16,11,6,7,8,9,14,13,12]
 };
 
+//fixtures for traverseCounterClockwise tests
+fixtures.traverseCounterClockwise = {};
+fixtures.traverseCounterClockwise.emptyMatrix = {
+  inputValue: [[]],
+  outputExpected: []
+};
+fixtures.traverseCounterClockwise.oneByOne = {
+  inputValue: [[1]],
+  outputExpected: [1]
+};
+fixtures.traverseCounterClockwise.oneRow = {
+  inputValue: [[1,2,3]],
+  outputExpected: [1,2,3]
+};
+fixtures.traverseCounterClockwise.oneColumn = {
+  inputValue: [[1],[2],[3]],
+  outputExpected: [1,2,3]
+};
+fixtures.traverseCounterClockwise.twoByTwo = {
+  inputValue: [
+    [1,2],
+    [3,4]
+  ],
+  outputExpected: [1,3,4,2]
+};
+fixtures.traverseCounterClockwise.threeByThree = {
+  inputValue: [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+  ],
+  outputExpected: [1,4,7,8,9,6,3,2,5]
+};
+fixtures.traverseCounterClockwise.threeByFour = {
+  inputValue: [
+    [1  ,2  ,3  ,4],
+    [5  ,6  ,7  ,8],
+    [9  ,10 ,11 ,12]
+  ],
+  outputExpected: [1,5,9,10,11,12,8,4,3,2,6,7]
+};
+fixtures.traverseCounterClockwise.fourByFour = {
+  inputValue: [
+    [1  ,2  ,3  ,4],
+    [5  ,6  ,7  ,8],
+    [9  ,10 ,11 ,12],
+    [13 ,14 ,15 ,16]
+  ],
+  outputExpected: [1,5,9,13,14,15,16,12,8,4,3,2,6,10,11,7]
+};
+fixtures.traverseCounterClockwise.fourByFive = {
+  inputValue: [
+    [1  ,2  ,3  ,4  ,5],
+    [6  ,7  ,8  ,9  ,10],
+    [11 ,12 ,13 ,14 ,15],
+    [16 ,17 ,18 ,19 ,20],
+  ],
+  outputExpected: [1,6,11,16,17,18,19,20,15,10,5,4,3,2,7,12,13,14,9,8]
+};
 
 
 module.exports = fixtures;
--- test/spiralTraversal_test.js
+++ test/spiralTraversal_test.js
@@ -319,4 +319,57 @@ describe('Spiral Traversal', function(){
       assert.deepEqual(result, expected);
     });
   });
+
+  describe('traverseCounterClockwise()', function(){
+    it('should exist', function(){
+      assert.doesNotThrow(function(){
+        spiral.traverseCounterClockwise([[]]);
+      });
+    });
+    it('should return an empty array if given an empty matrix', function(){
+      var result = spiral.traverseCounterClockwise(fixtures.traverseCounterClockwise.emptyMatrix.inputValue);
+      var expected = fixtures.traverseCounterClockwise.emptyMatrix.outputExpected;
+      assert.deepEqual(result, expected);
+    });
+    it('should return array with 1 element if given a 1x1 matrix', function(){
+      var result = spiral.traverseCounterClockwise(fixtures.traverseCounterClockwise.oneByOne.inputValue);
+      var expected = fixtures.traverseCounterClockwise.oneByOne.outputExpected;
+      assert.deepEqual(result, expected);
+    });
+    it('should return array with correct order and 3 elements if given a 1x3 matrix', function(){
+      var result = spiral.traverseCounterClockwise(fixtures.traverseCounterClockwise.oneRow.inputValue);
+      var expected = fixtures.traverseCounterClockwise.oneRow.outputExpected;
+      assert.deepEqual(result, expected);
+    });
+    it('should return array with correct order and 3 elements if given a 3x1 matrix', function(){
+      var result = spiral.traverseCounterClockwise(fixtures.traverseCounterClockwise.oneColumn.inputValue);
+      var expected = fixtures.traverseCounterClockwise.oneColumn.outputExpected;
+      assert.deepEqual(result, expected);
+    });
+    it('should return array with correct order and 4 elements if given a 2x2 matrix', function(){
+      var result = spiral.traverseCounterClockwise(fixtures.traverseCounterClockwise.twoByTwo.inputValue);
+      var expected = fixtures.traverseCounterClockwise.twoByTwo.outputExpected;
+      assert.deepEqual(result, expected);
+    });
+    it('should return array with correct order and 9 elements if given a 3x3 matrix', function(){
+      var result = spiral.traverseCounterClockwise(fixtures.traverseCounterClockwise.threeByThree.inputValue);
+      var expected = fixtures.traverseCounterClockwise.threeByThree.outputExpected;
+      assert.deepEqual(result, expected);
+    });
+    it('should return array with correct order and 9 elements if given a 3x4 matrix', function(){
+      var result = spiral.traverseCounterClockwise(fixtures.traverseCounterClockwise.threeByFour.inputValue);
+      var expected = fixtures.traverseCounterClockwise.threeByFour.outputExpected;
+      assert.deepEqual(result, expected);
+    });
+    it('should return array with correct order and 9 elements if given a 4x4 matrix', function(){
+      var result = spiral.traverseCounterClockwise(fixtures.traverseCounterClockwise.fourByFour.inputValue);
+      var expected = fixtures.traverseCounterClockwise.fourByFour.outputExpected;
+      assert.deepEqual(result, expected);
+    });
+    it('should return array with correct order and 9 elements if given a 4x5 matrix', function(){
+      var result = spiral.traverseCounterClockwise(fixtures.traverseCounterClockwise.fourByFive.inputValue);
+      var expected = fixtures.traverseCounterClockwise.fourByFive.outputExpected;
+      assert.deepEqual(result, expected);
+    });
+  });
 });
