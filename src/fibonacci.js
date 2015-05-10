8db5e9d45bd622f1f0f05082d974a05f62908853
Tony Rizko <tony@rizkocircle.com>
2015-05-09 19:14:18 -0700

    (pass)tests for fib iterative

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -6,4 +6,21 @@ fibonacci.recursion = function(n){
   return fibonacci.recursion(n-1) + fibonacci.recursion(n-2);
 };
 
+fibonacci.iterative = function(n){
+  if(n === 0){ return 0 };
+  if(n === 1){ return 1 };
+
+  var prev = 0
+  var current = 1;
+  var result = 0;
+
+  for(var i = 1; i < n; i++){
+    result = prev + current;
+    prev = current;
+    current = result;
+  }
+
+  return result;
+};
+
 module.exports = fibonacci;
