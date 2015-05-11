3a31cd4cbd823adae0521bfc12c0967b3581217c
Tony Rizko <tony@rizkocircle.com>
2015-05-11 09:34:45 -0700

    (test)for parseHour method

--- test/minClockAngle.js
+++ test/minClockAngle.js
@@ -1,2 +1,12 @@
 var fibonacci = require('../src/minClockAngle.js');
 var assert = require('assert');
+
+describe('Minimum 24 hour clock angle', function(){
+  describe('parseHour', function(){
+    it('should exist', function(){
+      assert.doesNotThrow(function(){
+        minClockAngle.parseHour("01:23");
+      });
+    });
+  });
+});
