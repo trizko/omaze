c73a629dbb8be4c59b52bc569f6ed0c156b51eb1
Tony Rizko <tony@rizkocircle.com>
2015-05-11 09:15:42 -0700

    (test)for memorized recursive solution

--- test/fibonacci_test.js
+++ test/fibonacci_test.js
@@ -25,6 +25,29 @@ describe('Fibonacci', function(){
     });
   });
 
+  describe('recursionMemorized', function(){
+    it('should exist', function(){
+      assert.doesNotThrow(function(){
+        fibonacci.recursionMemorized(0);
+      });
+    });
+    it('should return 5 if given 5 as nth input', function(){
+      var result = fibonacci.recursionMemorized(5);
+      var expected = 5;
+      assert.equal(result, expected);
+    });
+    it('should return 21 if given 8 as nth input', function(){
+      var result = fibonacci.recursionMemorized(8);
+      var expected = 21;
+      assert.equal(result, expected);
+    });
+    it('should return 144 if given 12 as nth input', function(){
+      var result = fibonacci.recursionMemorized(12);
+      var expected = 144;
+      assert.equal(result, expected);
+    });
+  });
+
   describe('iterative', function(){
     it('should exist', function(){
       assert.doesNotThrow(function(){
