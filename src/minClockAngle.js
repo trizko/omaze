b6017ed86d6b2bf85977df88d793f2c503b155d0
Tony Rizko <tony@rizkocircle.com>
2015-05-11 09:45:18 -0700

    (pass)tests for parseMinutes method

--- src/minClockAngle.js
+++ src/minClockAngle.js
@@ -4,4 +4,8 @@ minClockAngle.parseHour = function(time){
   return parseInt(time.split(':')[0]);
 };
 
+minClockAngle.parseMinutes = function(time){
+  return parseInt(time.split(':')[1]);
+};
+
 module.exports = minClockAngle;
